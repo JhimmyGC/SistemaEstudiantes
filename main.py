65e7d0f3b162069b7fb4316c9fa90a10bb0a3849
Jhimmy <pbbanger32@gmail.com>
2026-06-28 16:59:01 -0500

    agregando las apis de alumnos y cursos al archivo principal

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from src.router.estudiante import router as alumnos_router
+from src.router.curso import router as cursos_router
 
 app = FastAPI()
+app.include_router(alumnos_router)
+app.include_router(cursos_router)
 
 
 @app.get("/")
